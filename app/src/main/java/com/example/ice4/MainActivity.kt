de06ca0f0dda40c95c07bfd76c390d3378db581d
Olivia Guichard <ST10486576@vcconnect.edu.za>
2025-05-01 11:35:57 +0200

    Code for MainActivity completed

--- app/src/main/java/com/example/ice4/MainActivity.kt
+++ app/src/main/java/com/example/ice4/MainActivity.kt
@@ -1,18 +1,44 @@
 package com.example.ice4
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        //declare variables to be used in the code
+        var txtDisplay = findViewById<TextView>(R.id.txtDisplay)
 
+        //declare variables to be used in the code
+        var btnStart = findViewById<Button>(R.id.btnStart)
+
+        //displays the text in green when the app is run
+        txtDisplay.text="""
+            Welcome to the Movie quiz app!
+            The quiz is made up of 5 
+            true or false questions based on 
+            well known movies.
+            Press the start button to start the quiz.
+        """.trimIndent()
+
+        //switches to the movie page when the button is clicked
+        btnStart.setOnClickListener{
+            val intent = Intent(this, Question1::class.java)
+
+            //start Question1, which will open a new screen
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/example/ice4/Question1.kt
+++ app/src/main/java/com/example/ice4/Question1.kt
@@ -1,12 +1,16 @@
 package com.example.ice4
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Question1 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
