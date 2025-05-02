d8a4a46c8ddae001c3aba63a4670ac1adc649b68
Olivia Guichard <ST10486576@vcconnect.edu.za>
2025-05-02 07:12:49 +0200

    Design aspects updated + code for Question1.kt started

--- app/src/main/java/com/example/ice4/Question1.kt
+++ app/src/main/java/com/example/ice4/Question1.kt
@@ -16,7 +16,19 @@ class Question1 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_question1)
 
+        //declare variables to be used in the code
+        val txtDisplay = findViewById<TextView>(R.id.txtDisplay2)
 
+        //declare variables to be used in the code
+        val btnNext = findViewById<Button>(R.id.btnNext)
+
+        //declare variables to be used in the code
+        val btnTrue = findViewById<Button>(R.id.btnTrue)
+
+        //declare variables to be used in the code
+        val btnFalse = findViewById<Button>(R.id.btnFalse)
+
+        val questions = arrayOf("Question 1", "Question 2", "Question 3", "Question 4", "Question 5")
 
     }
 }
